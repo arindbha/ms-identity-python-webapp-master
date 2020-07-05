c0c375bad64502e52c502009124280f62090db54
Arindam Bhattacharjee <arindam1994@hotmail.com>
2020-07-05 18:04:51 +0530

    updated

--- app_config.py
+++ app_config.py
@@ -1,9 +1,9 @@
 import os
 
-CLIENT_ID = "e4db746d-7203-450c-9c10-12ea74faa2cc" # Application (client) ID of app registration
+CLIENT_ID = "" # Application (client) ID of app registration
 
-CLIENT_SECRET = "rOvpF.S93U-54QAhm-7l29I.To~Xsb~gsY" # Placeholder - for use ONLY during testing.
-AUTHORITY = "https://login.microsoftonline.com/25e42d7e-cebb-4dd3-9a15-6e3060e48ee2"  # For multi-tenant app
+CLIENT_SECRET = "" # Placeholder - for use ONLY during testing.
+AUTHORITY = "https://login.microsoftonline.com/tenant_id"  # For multi-tenant app
 REDIRECT_PATH = "/getAToken"
 ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
 SCOPE = ["User.ReadBasic.All"]
